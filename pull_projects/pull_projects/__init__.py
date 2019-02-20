7647c3c472b17d9cc79081c23966b7318b9dee3e
Ubuntu <ubuntu@ip-172-31-26-145.ec2.internal>
2019-02-20 17:12:15 +0000

    update projects extension

--- pull_projects/pull_projects/__init__.py
+++ pull_projects/pull_projects/__init__.py
@@ -1,9 +1,34 @@
-# Running `npm run build` will create static resources in the static
-# directory of this Python package (and create that directory if necessary).
+__version__ = '0.1.0'
+import os
+import os.path
+from notebook.utils import url_path_join
 
+from .handlers import ListProjectsHandler, GetProjectHandler, GetAllProjectsHandler
 
-def _jupyter_labextension_paths():
+def _jupyter_server_extension_paths():
     return [{
-        'name': 'pull_projects',
-        'src': 'static',
+        "module": "pull_projects"
     }]
+
+
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    web_app = nb_server_app.web_app
+    base_url = web_app.settings['base_url']
+    host_pattern = '.*$'
+
+
+    print('Installing jupyterlab_projects handler on path %s' % url_path_join(base_url, 'projects'))
+    print('base_url is '+base_url)
+
+    web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'pull_projects/listProjects'), ListProjectsHandler)])
+    web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'pull_projects/getProject'), GetProjectHandler)])
+    web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'pull_projects/getProject'), GetAllProjectsHandler)])
+    # web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'pull_projects/putProject'), PutProjectHandler)])
+    # web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'pull_projects/deleteProject'), DeleteProjectsHandler)])
+
+    print(ListProjectsHandler().get())
--- pull_projects/pull_projects/handlers.py
+++ pull_projects/pull_projects/handlers.py
@@ -2,92 +2,78 @@ import os.path
 import sys
 import nbformat
 from notebook.base.handlers import IPythonHandler
-import subprocess
-sys.path.append('./search/maap-py')
-print(subprocess.check_output('pwd',shell=True))
-#from maap import maap.MAAP as MAAP
-import maap
-from maap.maap import MAAP
+
+import os
+import requests
+import json
+from git import Repo
+import shutil
+import urllib
 
 # Set selected ADE Docker Image 
-class GetCollectionsHandler(IPythonHandler):
-    def printResultsToString(self,results):
-        datasets = ''
-        for res in results:
-            datasets += ("Short Name: " + res['Collection']['ShortName'] + '\n')
-            datasets += ("Dataset ID: " + res['Collection']['DataSetId'] + '\n')
-            if 'Collection' in res and 'ArchiveCenter' in res['Collection']:
-                datasets += ("Archive Center: " + res['Collection']['ArchiveCenter'] + '\n')
-            if res['Collection']['Description'] is not None:
-                datasets += ("Description: " + res['Collection']['Description'] + '\n')
-            # if 'BoundingRectangle' in res['Collection']['Spatial']['HorizontalSpatialDomain']['Geometry'] is not None:
-            #     datasets += ("Bounding Rectangle: " + str(res['Collection']['Spatial']['HorizontalSpatialDomain']['Geometry']['BoundingRectangle']) + '\n')
-            datasets += "================================================\n"
-        # print(datasets)
-        return datasets
-
+class ListProjectsHandler(IPythonHandler):
     def get(self):
-        #print(subprocess.check_output('ls',shell=True))
-        # do stuff
-        maap = MAAP('./search/maap-py/maap.cfg')
-        #maap = MAAP('/home/ubuntu/extensions/search/search/maap-py/maap.cfg')
-
-        query_result = ''
-
-        try:
-            kw = self.get_argument('keyword', '')
-            results = maap.searchCollection(keyword=kw)
-            query_result = self.printResultsToString(results)
-            # query_result = results
-        except:
-            kw = ''
-
-
-        try:
-            instr = self.get_argument('instrument', '')
-            results = maap.searchCollection(instrument=instr)
-            url_list = ''
-            for res in results:
-                url_list = url_list + res.getDownloadUrl() + '\n'
-            query_result = url_list
-        except:
-            instr = ''
-
-
-        #results = maap.searchCollection(keyword='precipitation')
-        #granules = maap.searchGranule(short_name='MOD11A1')
-        self.finish({"collections_found": query_result})
-
-
-class GetGranulesHandler(IPythonHandler):
-    def printUrls(self, granules):
-        url_list = '[\n'
-        for res in granules:
-            url_list = url_list + '\'' + res.getDownloadUrl() + '\',\n'
-        url_list = url_list + ']'
-        return url_list
-
-    def get(self):
-        #print(subprocess.check_output('ls',shell=True))
-        # do stuff
-        maap = MAAP('./search/maap-py/maap.cfg')
-        #maap = MAAP('/home/ubuntu/extensions/search/search/maap-py/maap.cfg')
-
-        query_result = ''
+        # 'https://che-k8s.maap.xyz/api/workspace/workspacetn41o4yl4a7kxclz'
+        workspace_id = os.environ['CHE_WORKSPACE_ID']
+        che_machine_token = os.environ['CHE_MACHINE_TOKEN']
+        url = 'https://che-k8s.maap.xyz/api/workspace/{workspace_id}'.format(workspace_id=workspace_id)
+        # --------------------------------------------------
+        # TODO: FIGURE OUT AUTH KEY & verify
+        # --------------------------------------------------
+        headers = {
+            'Accept':'application/json',
+            'Authorization':'Bearer {token}'.format(token=che_machine_token)
+        }
+        r = requests.get(
+            url, 
+            headers=headers, 
+            verify=False
+        )
 
         try:
-            instr = self.get_argument('instrument', '')
-            site = self.get_argument('sitename', '')
-            granules = maap.searchGranule(sitename=site, instrument=instr)
-            # print(granules)
-            # url_list = ''
-            # for res in granules:
-            #     url_list = url_list + res.getDownloadUrl() + '\n'
-            query_result = self.printUrls(granules)
+            resp = json.loads(r.text)               # JSON response to dict
+            projects = resp['config']['projects']   # gets list of projects, each is dict with project properties
+
+            self.finish(json.dumps(projects))
         except:
-            instr = ''
+            self.finish({"status_code": r.status_code, "reason": r.reason})
 
+class GetProjectHandler(IPythonHandler):
+    def get(self, project_name, location, src_type):
+        # ws_agent_port = 3100
+        # project_name = "jupyterlab-logout"
+        # import_type = 'git'                                         # can be 'git' | 'svn' | 'zip'
+        # location = 'https://github.com/zgqallen/jupyterlab-logout'  # location where project can be downloaded from
+                
+        # che_machine_token = os.environ['CHE_MACHINE_TOKEN'] 
+        # # --------------------------------------------------
+        # # TODO: FIGURE OUT AUTH KEY & verify
+        # # --------------------------------------------------
+        # headers = {
+        #     'Accept':'application/json',
+        #     'Authorization':'Bearer {token}'.format(token=che_machine_token)
+        # }
+        # url = "http://localhost:{ws_agent_port}/wsagent/ext/project/import/{project_name}".format(ws_agent_port=str(ws_agent_port), project_name=project_name)
+        # r = requests.post(url,headers=headers,data = {"location":"{location}".format(location=location), "type":"{import_type}".format(import_type=import_type)}, verify=False)
+        dl_loc = '/projects/'+project_name
+        
+        try:
+            if src_type == 'git':
+                Repo.clone_from(location,dl_loc)
+            elif src_type == 'zip':
+                with urllib.urlopen(location) as response, open(dl_loc, 'w+') as out_file:
+                    shutil.copyfileobj(response, out_file)
+            self.finish({"message": "project import done"})
+        except:
+            self.finish({"message": "project import failed"})
+            
+class GetAllProjectsHandler(IPythonHandler):
+    def __init__(self):
+        self.getproject_handler = GetProjectHandler()
 
-        #results = maap.searchCollection(keyword='precipitation')
-        #granules = maap.searchGranule(short_name='MOD11A1')
-        self.finish({"granule_urls": query_result})
+    def get(self,projects_json):
+        projects_list = json.loads(projects_json)
+        for project in projects_list:
+            self.getproject_handler.get(project['name'],project['source']['location'],project['source']['type'])
+        self.finish({"message": "done importing projects"})
+
