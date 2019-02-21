8b01e5ab838f94a4533f3b0c9ccf23723d98d532
Debellis <maya.debellis@jpl.nasa.gov>
2019-02-21 13:55:22 -0800

    delete renamed extension

--- display_ssh_info/display_ssh_info/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.1.0'
-
-
-def _jupyter_server_extension_paths():
-    return [{
-        "module": "display_ssh_info"
-    }]
--- display_ssh_info/tests/test_all.py
+++
@@ -1,3 +0,0 @@
-# for Coverage
-from jupyterlab_autoversion import *
-from jupyterlab_autoversion.extension import *
