12255b4f6e3fcd5f1724e6fda6a26aa5f13fa186
echyam <elizabeth.yam@jpl.nasa.gov>
2019-03-07 10:34:09 -0800

    stop freezing on load

--- pull_projects/lib/plugin.js
+++
@@ -1,8 +0,0 @@
-module.exports = [{
-    id: 'pull_projects',
-    autoStart: true,
-    activate: function(app) {
-      console.log('JupyterLab extension pull_projects is activated!');
-      console.log(app.commands);
-    }
-}];
