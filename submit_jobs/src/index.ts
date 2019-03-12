bbec1e87216878c744d9b877d2adf198ea5a664c
echyam <echyam@gmail.com>
2019-03-12 10:53:23 -0700

    5 widgets for 5 different dps calls

--- submit_jobs/src/index.ts
+++ submit_jobs/src/index.ts
@@ -11,14 +11,34 @@ import { ILauncher } from '@jupyterlab/launcher';
 class HySDSWidget extends Widget {
 
   // TODO: protect instance vars
+  public readonly req: string;
   public response_text: string;
   public fields: string[];
 
-  constructor() {
+  constructor(req:string) {
     super();
      // Default text
     this.response_text = "";
-    this.fields = ['Title', 'Identifier', 'Metadata'];
+    this.req = req;
+
+    switch (req) {
+      case 'getCapabilities':
+        console.log('getCapabilities');
+        break;
+      case 'getStatus':
+        console.log('getStatus');
+        break;
+      case 'getResult':
+        console.log('getResult');
+        break;
+      case 'execute':
+        this.fields = ['Title', 'Identifier', 'Metadata'];
+        console.log('execute');
+        break;
+      case 'dismiss':
+        console.log('dismiss');
+        break;
+    }
 
     // bind method definitions of "this" to refer to class instance
     this.getValue = this.getValue.bind(this);
@@ -80,7 +100,7 @@ class HySDSWidget extends Widget {
   // overrides the resolution of popup dialog
   getValue() {
     // var me = this;
-    var getUrl = new URL(PageConfig.getBaseUrl() + 'hysds/submit'); // REMINDER: hack this url until fixed
+    var getUrl = new URL(PageConfig.getBaseUrl() + 'hysds/'+this.req); // REMINDER: hack this url until fixed
 
     for (var field of this.fields) {
       var fieldText = (<HTMLInputElement>document.getElementById(field.toLowerCase()+'-input')).value;
@@ -125,41 +145,117 @@ export function popup(b:any): void {
   });
 }
 
-export function activate(app: JupyterLab, 
+export function activateGetCapabilities(app: JupyterLab, 
                         palette: ICommandPalette, 
                         restorer: ILauncher | null): void{
-  
-  // add application command
-  const open_command = 'hysds: submit-job';
+  const open_command = 'hysds: get-capabilities';
 
   app.commands.addCommand(open_command, {
-    label: 'Submit Job to DPS',
+    label: 'Get DPS Capabilities',
     isEnabled: () => true,
     execute: args => {
-      popup(new HySDSWidget());
+      popup(new HySDSWidget('getCapabilities'));
     }
   });
-
   palette.addItem({command: open_command, category: 'DPS'});
+  console.log('HySDS Get Capabilities is activated!');
+}
+export function activateGetStatus(app: JupyterLab, 
+                        palette: ICommandPalette, 
+                        restorer: ILauncher | null): void{  
+  const open_command = 'hysds: get-status';
 
-  console.log('HySDS Job Submit is activated!');
+  app.commands.addCommand(open_command, {
+    label: 'Get DPS Job Status',
+    isEnabled: () => true,
+    execute: args => {
+      popup(new HySDSWidget('getStatus'));
+    }
+  });
+  palette.addItem({command: open_command, category: 'DPS'});
+  console.log('HySDS Get Job Status is activated!');
+}
+export function activateGetResult(app: JupyterLab, 
+                        palette: ICommandPalette, 
+                        restorer: ILauncher | null): void{
+  const open_command = 'hysds: get-result';
 
-  // add as side panel
-  // var infoPanel = new HySDSWidget();
-  // infoPanel.id = 'submit-job';
-  // infoPanel.title.label = 'Submit Job';
-  // infoPanel.title.caption = 'submit job to HySDS';
+  app.commands.addCommand(open_command, {
+    label: 'Get DPS Job Result',
+    isEnabled: () => true,
+    execute: args => {
+      popup(new HySDSWidget('getResult'));
+    }
+  });
+  palette.addItem({command: open_command, category: 'DPS'});
+  console.log('HySDS Get Job Result is activated!');
+}
+export function activateExecute(app: JupyterLab, 
+                        palette: ICommandPalette, 
+                        restorer: ILauncher | null): void{
+  const open_command = 'hysds: execute-job';
 
-  // app.shell.addToLeftArea(infoPanel, {rank:300});
+  app.commands.addCommand(open_command, {
+    label: 'Execute DPS Job',
+    isEnabled: () => true,
+    execute: args => {
+      popup(new HySDSWidget('execute'));
+    }
+  });
+  palette.addItem({command: open_command, category: 'DPS'});
+  console.log('HySDS Execute Job is activated!');
+}
+export function activateDismiss(app: JupyterLab, 
+                        palette: ICommandPalette, 
+                        restorer: ILauncher | null): void{
+  const open_command = 'hysds: dismiss-job';
+
+  app.commands.addCommand(open_command, {
+    label: 'Dismiss DPS Job',
+    isEnabled: () => true,
+    execute: args => {
+      popup(new HySDSWidget('dismiss'));
+    }
+  });
+  palette.addItem({command: open_command, category: 'DPS'});
+  console.log('HySDS Dismiss Job is activated!');
 }
 
 // export extensions
-const extension: JupyterLabPlugin<void> = {
-  id: 'submit-job',
+const extensionCapabilities: JupyterLabPlugin<void> = {
+  id: 'dps-capabilities',
   autoStart: true,
-  requires: [ ICommandPalette],
+  requires: [ICommandPalette],
   optional: [ILauncher],
-  activate: activate
+  activate: activateGetCapabilities
+};
+const extensionStatus: JupyterLabPlugin<void> = {
+  id: 'dps-job-status',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ILauncher],
+  activate: activateGetStatus
+};
+const extensionResult: JupyterLabPlugin<void> = {
+  id: 'dps-job-result',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ILauncher],
+  activate: activateGetResult
+};
+const extensionExecute: JupyterLabPlugin<void> = {
+  id: 'dps-job-execute',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ILauncher],
+  activate: activateExecute
+};
+const extensionDismiss: JupyterLabPlugin<void> = {
+  id: 'dps-job-dismiss',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ILauncher],
+  activate: activateDismiss
 };
 
-export default extension;
+export default [extensionCapabilities,extensionStatus,extensionResult,extensionExecute,extensionDismiss];
