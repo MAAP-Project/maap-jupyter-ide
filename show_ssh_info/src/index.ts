9a50a1437dcf45972f671e12ce16eaf38b36d345
Debellis <maya.debellis@jpl.nasa.gov>
2019-03-13 14:48:42 -0700

    updating show ssh info to allow users to uplaod their own key, this is not really relevant anymore

--- show_ssh_info/src/index.ts
+++ show_ssh_info/src/index.ts
@@ -60,7 +60,7 @@ class SshWidget extends Widget {
       if(res.ok){
         let json_results:any = res.json();
         let ip = json_results['ip'];
-        let port = json_results['port']
+        let port = json_results['port'];
         let message = "ssh -i <path_to_your_key> root@" + ip + " -p " + port;
         let contents = document.createTextNode(message);
         body.appendChild(contents);
@@ -76,12 +76,54 @@ class SshWidget extends Widget {
 }
 
 export
+class InputKeyWidget extends Widget {
+  constructor() {
+    let body = document.createElement('div');
+    body.style.display = 'flex';
+    body.style.flexDirection = 'column';
+    let textarea = document.createElement("TEXTAREA");
+    textarea.id = 'public-key-text';
+    console.log((<HTMLTextAreaElement>textarea).value)
+    body.appendChild(textarea)
+
+    // let type = document.createElement('select');
+
+    // type.style.marginBottom = '15px';
+    // type.style.minHeight = '25px';
+    // body.appendChild(type);
+
+    super({ node: body });
+  }
+
+}
+
+export
 function autoversion(): void {
     showDialog({
         title: 'SSH Info:',
         body: new SshWidget(),
         focusNodeSelector: 'input',
-        buttons: [Dialog.okButton({ label: 'Ok' })]
+        buttons: [Dialog.okButton({ label: 'Upload Public Key' }), Dialog.okButton({ label: 'Ok' })]
+    }).then(result => {
+        if (result.button.label === 'Upload Public Key') {
+
+            showDialog({
+                title: 'Upload Public Key:',
+                body: new InputKeyWidget(),
+                focusNodeSelector: 'input',
+                buttons: [Dialog.okButton({ label: 'Ok' })]
+            }).then(result => {
+                console.log(result.value)
+                console.log("take in input here");
+                let test = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOBk5VU/BPAwsQjBnoMFGRptPR0vB1A2nWgc9Fz0BbgCRYFwdhPJM2IgiV4xmj6On3wcvr+RQ7i64gBCNM2UxZiGeX9b/bVfoMLJMQBCHAW/UH9Oxssa0wBHoCfhVrl3LoC+Esy3hSU05g95VwFMxxqN6tF20JpA6lDCYJtTrpCodlDVVt5ESl/BQcYy6YqSV8INITiLB9aN/SU/Ns9FxIoTu7o/pZrcOZVZfNVLAwkCwNmika05vJJCpCMe/f4PK81QN/WNJZAVbMq7HISg6SNxaz5uFX9Umhpgnb9RXYSUImS08/JTnk+Z3JpD1Q3V9jUs3PLhimzkrqcVgk0Rhn"
+                request('get', PageConfig.getBaseUrl() + "show_ssh_info/addKey", {'public_key': test})
+                    .then((res: RequestResult) => {
+                        if (res.ok) {
+                            console.log("added the key!")
+                        }
+                    });
+            });
+        }
     });
 
 }
