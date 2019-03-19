176ad888c1c742a81805c6a0634b80438067ba03
echyam <echyam@gmail.com>
2019-03-19 09:27:27 -0400

    generalize fields getter

--- submit_jobs/submit_jobs/fields.py
+++ submit_jobs/submit_jobs/fields.py
@@ -1,28 +1,12 @@
 import json
 
 fields_path = './src/fields.json'
+# getCapabilities, execute, getStatus, getResult, dismiss, describe
 
-def getCapabilitiesFields():
-	with open(fields_path) as fields:
-		data = json.load(fields)
-		return data['getCapabilities']
-
-def executeFields():
-	with open(fields_path) as fields:
-		data = json.load(fields)
-		return data['execute']
-
-def getStatusFields():
-	with open(fields_path) as fields:
-		data = json.load(fields)
-		return data['getStatus']
-
-def getResultFields():
-	with open(fields_path) as fields:
-		data = json.load(fields)
-		return data['getResult']
-
-def dismissFields():
-	with open(fields_path) as fields:
-		data = json.load(fields)
-		return data['dismiss']
+def getFields(param):
+	try:
+		with open(fields_path) as fields:
+			data = json.load(fields)
+			return data[param]
+	except:
+		return []
