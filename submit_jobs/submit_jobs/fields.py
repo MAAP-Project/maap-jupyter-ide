fed76c4c9e91841b9da6d96adf3f3e3bc04730d9
echyam <echyam@gmail.com>
2019-03-28 12:37:13 -0700

    fix paths for che

--- submit_jobs/submit_jobs/fields.py
+++ submit_jobs/submit_jobs/fields.py
@@ -1,6 +1,6 @@
 import json
 
-fields_path = './src/fields.json'
+fields_path = '../submit_jobs/src/fields.json'
 # getCapabilities, execute, getStatus, getResult, dismiss, describe
 
 def getFields(param):
