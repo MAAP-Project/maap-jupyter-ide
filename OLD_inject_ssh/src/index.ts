90396161bbaa9968adb4f33c7570a1256de7bb41
Debellis <mayadebellis@gmail.com>
2019-04-04 18:01:56 -0700

    .js files in OLD now can function as a module to get keycloak info

--- OLD_inject_ssh/src/index.ts
+++ OLD_inject_ssh/src/index.ts
@@ -1,16 +1,16 @@
 import { ICommandPalette } from '@jupyterlab/apputils';
-import { PageConfig } from '@jupyterlab/coreutils'
+// import { PageConfig } from '@jupyterlab/coreutils'
 import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
 import { ILauncher } from '@jupyterlab/launcher';
 import { Widget } from '@phosphor/widgets';
-import { request, RequestResult } from './request';
+// import { request, RequestResult } from './request';
 
 // /// <reference path="./getKeycloak.d.ts" />
 // import * as getKeycloak from "./getKeycloak";
-// import getKeycloak = require("./getKeycloak");
+import getKeycloak = require("./getKeycloak");
 
 //declare var _keycloak: any;
-declare var Window: any;
+// declare var Window: any;
 
 // declare global {
 //     interface Window {
@@ -21,39 +21,43 @@ declare var Window: any;
 export class InjectSSH extends Widget {
   constructor() {
 
-    let key = "err";
-    console.log(Window);
-    //console.log(_keycloak);
-    console.log(Window._keycloak);
-    Window._keycloak.loadUserInfo().success(function(profile:any) {
-      console.log(profile);
-      key = profile['public_ssh_keys'];
-    }).error(function() {
-      console.log('Failed to load profile.');
-    });
-    //let profile = getKeycloak.getKeycloak();
-    //let key = "error";
-    // if (profile != "error") {
+    // let key = "err";
+    // console.log(Window);
+    // //console.log(_keycloak);
+    // console.log(Window._keycloak);
+    // Window._keycloak.loadUserInfo().success(function(profile:any) {
+    //   console.log(profile);
     //   key = profile['public_ssh_keys'];
-    // }
+    // }).error(function() {
+    //   console.log('Failed to load profile.');
+    // });
+    let profile = getKeycloak.getUserInfo();
+    let key = "error";
+    if (profile != "error") {
+      key = profile['public_ssh_keys'];
+      console.log(profile)
+      console.log(key)
+    }
 
-    // let token = Window._keycloak.token;
-    // console.log(token)
+    let token = getKeycloak.getToken();
+    console.log(token);
+
+
 
     let body = document.createElement('div');
     body.style.display = 'flex';
     body.style.flexDirection = 'column';
-    console.log("going to call backend in key injection");
-    request('get', PageConfig.getBaseUrl() + "OLD_inject_ssh/inject_public_key", {"key": key})
-        .then((res: RequestResult) => {
-      if(res.ok){
-        console.log("SSH Key injected")
-        // let json_response:any = res.json();
-        // let message = json_response['status'];
-        // let contents = document.createTextNode(message);
-        // body.appendChild(contents);
-      }
-    });
+    // console.log("going to call backend in key injection");
+    // request('get', PageConfig.getBaseUrl() + "OLD_inject_ssh/inject_public_key", {"key": key})
+    //     .then((res: RequestResult) => {
+    //   if(res.ok){
+    //     console.log("SSH Key injected")
+    //     // let json_response:any = res.json();
+    //     // let message = json_response['status'];
+    //     // let contents = document.createTextNode(message);
+    //     // body.appendChild(contents);
+    //   }
+    // });
     super({ node: body });
   }
 }
