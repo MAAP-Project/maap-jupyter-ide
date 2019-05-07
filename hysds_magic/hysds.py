23db8b53d26a63401e51b883b1e8b0b90d635b76
echyam <echyam@gmail.com>
2019-05-07 16:09:46 -0700

    separate endpoints, add status & result

--- hysds_magic/hysds.py
+++ hysds_magic/hysds.py
@@ -1,12 +1,14 @@
 from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 import requests
+import json
 
 @magics_class
 class HysdsMagic(Magics):
 
     @line_magic
     def execute(self, line):
-        lk = 'http://localhost:8888/hysds/execute?'
+        lk = 'http://localhost:8888'
+        endpoint = '/hysds/execute'
         id = 'org.n52.wps.server.algorithm.SimpleBufferAlgorithm'
         algo_ver,params = line.split('(')
         params = params[:-1]
@@ -16,15 +18,38 @@ class HysdsMagic(Magics):
             k,v = kvpair.split('=')
             inputs.append(k)
         inputs.append('')
-        call = 'algo_id={algo}&version={ver}&identifier={id}&inputs={inputs}&{params}'.format(algo=algo,ver=ver,id=id,inputs=','.join(inputs),params=params.replace(',','&'))
-        url = lk + call
+        call = '?algo_id={algo}&version={ver}&identifier={id}&inputs={inputs}&{params}'.format(algo=algo,ver=ver,id=id,inputs=','.join(inputs),params=params.replace(',','&'))
+        url = lk + endpoint + call
         r = requests.get(url)
-        print('{}\n{}'.format(url,r.text))
-        return 
+        resp = json.loads(r.text)
+        print('{}\n{}'.format(url,resp['result']))
+        return
+    
+    @line_magic
+    def status(self, line):
+        lk = 'http://localhost:8888'
+        endpoint = '/hysds/getStatus'
+        call = '?job_id={}'.format(line.strip())
+        url = lk + endpoint + call
+        r = requests.get(url)
+        resp = json.loads(r.text)
+        print('{}\n{}'.format(url,resp['result']))
+        return
+    
+    @line_magic
+    def result(self, line):
+        lk = 'http://localhost:8888'
+        endpoint = '/hysds/getResult'
+        call = '?job_id={}'.format(line.strip())
+        url = lk + endpoint + call
+        r = requests.get(url)
+        resp = json.loads(r.text)
+        print('{}\n{}'.format(url,resp['result']))
+        return
 
-    @cell_magic
-    def cadabra(self, line, cell):
-        return line, cell
+#     @cell_magic
+#     def cadabra(self, line, cell):
+#         return line, cell
 
 # @register_line_magic
 # def execute(line):
