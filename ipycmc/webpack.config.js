589b6af415972363b0971eff3d520b4d5a07a826
Flynn Platt <flynn.platt@jpl.nasa.gov>
2019-05-16 11:06:54 -0700

    update package names and locations

--- ipycmc/webpack.config.js
+++ ipycmc/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const version = require("./package.json").version;
 
 // cmc-jupyter assets
-cmc_assets = /common-mapping-client-jupyter\/dist\/assets/;
+cmc_assets = /maap-common-mapping-client\/dist\/assets/;
 
 // Custom webpack rules
 const rules = [
