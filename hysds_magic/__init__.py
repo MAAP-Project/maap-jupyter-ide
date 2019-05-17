651fadd9d7f9503caf55f8e24e0b997dff0abb13
echyam <echyam@gmail.com>
2019-05-17 13:20:04 -0700

    rm hysds_magic - working version in dps_magic

--- hysds_magic/__init__.py
+++
@@ -1,8 +0,0 @@
-from .hysds import HysdsMagic
-
-# def load_ipython_extension(ipython):
-#     ipython.register_magic_function(execute, 'line')
-
-def load_ipython_extension(ipython):
-	m = HysdsMagic()
-    ipython.register_magics(m)
