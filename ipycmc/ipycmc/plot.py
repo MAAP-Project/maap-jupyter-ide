f11eb610541c0d6f3a09ae39d1782fbad48eca95
Flynn Platt <flynn.platt@jpl.nasa.gov>
2019-06-06 17:25:42 -0700

    better random

--- ipycmc/ipycmc/plot.py
+++ ipycmc/ipycmc/plot.py
@@ -1,7 +1,7 @@
 import requests
 import plotly
 from plotly.offline import iplot
-from random import seed, randint
+from random import randint
 
 def retrieve_data(plot_type, start_date, end_date, layer_ids, geometry):
     req = requests.get('https://api.github.com/events')
@@ -9,7 +9,6 @@ def retrieve_data(plot_type, start_date, end_date, layer_ids, geometry):
     return resp
 
 def plot_data(plot_type="timeseries", data={}):
-    seed(3)
     sample_data = [randint(0,100) for x in range(100)]
     trace = plotly.graph_objs.Scatter(y=sample_data)
     fig = dict(data=[trace])
