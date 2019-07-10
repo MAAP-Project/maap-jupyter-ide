dc92836677d7a33210b64edfd6a14a3be59ca21f
Flynn Platt <flynn.platt@jpl.nasa.gov>
2019-07-10 08:40:12 -0700

    fix quotes

--- ipycmc/src/plot.ts
+++ ipycmc/src/plot.ts
@@ -10,7 +10,7 @@ export function generatePlotCommand(commandInfo: {
     const { plotType, startDate, endDate, geometry, datasets } = commandInfo;
     return [
         `# Initialize parameter variables`,
-        `plotType = ${plotType}`,
+        `plotType = "${plotType}"`,
         `startDate = "${moment(startDate)
             .utc()
             .toISOString()}"`,
