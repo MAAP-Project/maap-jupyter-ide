95c3de75e2b37d282080ec33a89a89b0ac8e19b4
echyam <echyam@gmail.com>
2019-11-04 10:16:59 -0800

    dps doesnt have a plugin

--- dps_magic/src/index.ts
+++ dps_magic/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Elizabeth Yam
 // Distributed under the terms of the Modified BSD License.
 
-export * from './version';
-// export * from './widget';
-export * from './plugin';
+// export * from './version';
+// // export * from './widget';
+// export * from './plugin';
