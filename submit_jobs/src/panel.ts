c2daa9c560f6ea0e840f612534d737ce49858841
echyam <elizabeth.yam@jpl.nasa.gov>
2020-01-07 19:45:55 -0800

    update helpers find element by id

--- submit_jobs/src/panel.ts
+++ submit_jobs/src/panel.ts
@@ -62,8 +62,9 @@ export class JobWidget extends Widget {
     tabs.appendChild(infoTab);
     job_widget.appendChild(tabs);
 
-    this._populateRunJobs(job_widget);
-    this._populateJobInfo(job_widget);
+    this._updateListCol();
+    this._updateExecuteCol();
+    this._updateOverviewCol();
 
     this.node.appendChild(job_widget);
   }
