85248d862968e56270e886c4ba99bd9d293c443f
echyam <echyam@gmail.com>
2020-05-19 14:25:10 -0700

    submit jobs request propagate returned  error messages

--- submit_jobs/src/widgets.ts
+++ submit_jobs/src/widgets.ts
@@ -338,7 +338,7 @@ export class InputWidget extends Widget {
             // console.log(json_response);
             me._responseText = me._responseText + '\n' + json_response['result'];
           } else {
-            me._responseText = "Error Sending Request.";
+            me._responseText = "Error Sending Request:\n" + json_response['result'];
           }
           console.log("updating");
           me.updateSearchResults();
