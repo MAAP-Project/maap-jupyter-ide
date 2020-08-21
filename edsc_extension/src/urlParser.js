e67ad5f8530683f1391984c376f9deb08cd7a7a6
Maya DeBellis <mayadebellis@gmail.com>
2020-08-21 14:05:36 -0700

    Release 2.3! (#67)
    * Dev docs (#64)
    * update dev docs for each extension
    * submit_jobs minor cleanup and update CAS profile ticket attribute name
    * update labextension install and Che stack config
    * fix missing refs in submit jobs handler, return basic joblist if DPS doesn't give job details
    * fix dps panel, dps UI widget for jlab2
    * add installation list and dependencies to README
    * readme type, put maap-py library installation note before serverextension install commands
    * dynamically get username from profile in dps info
    * allow edsc to open on localhost
    * Update edsc_extension to work with new edsc instance (#66)
    * add url parsers from earthdata src code
    * add attribution comments
    * clean up
    * create cmr query url from edsc code, pass to maap-py
    * start granule/collection search
    * exclude working! showing correct params. list comment w search
    * collection and granule search working
    * cleanup, adding sources
    * use generalized helper to get profile for registration
    * fix some UI formatting
    * fix merge conflicts on readme and dps_info jobinfo from master
    Co-authored-by: bsatoriu <brian.p.satorius@jpl.nasa.gov>
    Co-authored-by: Sujen Shah <sujen1412@gmail.com>
    Co-authored-by: echyam <echyam@gmail.com>
    Co-authored-by: mayadebellis <mayadebellis@gmail.com>

---
+++ edsc_extension/src/urlParser.js
@@ -0,0 +1,162 @@
+/**
+ *
+ * Url Decoder copied from earthdata search source code
+ * https://github.com/nasa/earthdata-search/blob/master/static/src/js/util/url/url.js#L78
+ *
+ * June 30, 2020 - https://github.com/nasa/earthdata-search/releases/tag/v1.123.14
+ *
+ */
+
+
+import qs from 'qs'
+
+import { decodeFeatures, encodeFeatures } from './encodersDecoders'
+import { decodeFacets, encodeFacets } from './encodersDecoders'
+import { decodeMap, encodeMap } from './encodersDecoders'
+import { decodeScienceKeywords, encodeScienceKeywords } from './encodersDecoders'
+import { decodeString, encodeString } from './encodersDecoders'
+import { decodeTemporal, encodeTemporal } from './encodersDecoders'
+import { decodeTimeline, encodeTimeline } from './encodersDecoders'
+import { decodeCollections, encodeCollections } from './encodersDecoders'
+import { decodeGridCoords, encodeGridCoords } from './encodersDecoders'
+import { decodeHasGranulesOrCwic, encodeHasGranulesOrCwic } from './encodersDecoders'
+import { encodeAdvancedSearch, decodeAdvancedSearch } from './encodersDecoders'
+import { encodeAutocomplete, decodeAutocomplete } from './encodersDecoders'
+
+/**
+ * Mapping of URL Shortened Keys to their redux store keys
+ */
+const urlDefs = {
+    focusedGranule: { shortKey: 'g', encode: encodeString, decode: decodeString },
+    keywordSearch: { shortKey: 'q', encode: encodeString, decode: decodeString },
+    pointSearch: { shortKey: 'sp', encode: encodeString, decode: decodeString },
+    boundingBoxSearch: { shortKey: 'sb', encode: encodeString, decode: decodeString },
+    polygonSearch: { shortKey: 'polygon', encode: encodeString, decode: decodeString },
+    lineSearch: { shortKey: 'line', encode: encodeString, decode: decodeString },
+    circleSearch: { shortKey: 'circle', encode: encodeString, decode: decodeString },
+    map: { shortKey: 'm', encode: encodeMap, decode: decodeMap },
+    temporalSearch: { shortKey: 'qt', encode: encodeTemporal, decode: decodeTemporal },
+    overrideTemporalSearch: { shortKey: 'ot', encode: encodeTemporal, decode: decodeTemporal },
+    featureFacets: { shortKey: 'ff', encode: encodeFeatures, decode: decodeFeatures },
+    platformFacets: { shortKey: 'fp', encode: encodeFacets, decode: decodeFacets },
+    instrumentFacets: { shortKey: 'fi', encode: encodeFacets, decode: decodeFacets },
+    organizationFacets: { shortKey: 'fdc', encode: encodeFacets, decode: decodeFacets },
+    projectFacets: { shortKey: 'fpj', encode: encodeFacets, decode: decodeFacets },
+    processingLevelFacets: { shortKey: 'fl', encode: encodeFacets, decode: decodeFacets },
+    granuleDataFormatFacets: { shortKey: 'gdf', encode: encodeFacets, decode: decodeFacets },
+    gridName: { shortKey: 's2n', encode: encodeString, decode: decodeString },
+    gridCoords: { shortKey: 's2c', encode: encodeGridCoords, decode: decodeGridCoords },
+    shapefileId: { shortKey: 'sf', encode: encodeString, decode: decodeString },
+    tagKey: { shortKey: 'tag_key', encode: encodeString, decode: decodeString },
+    hasGranulesOrCwic: { shortKey: 'ac', encode: encodeHasGranulesOrCwic, decode: decodeHasGranulesOrCwic },
+    autocompleteSelected: { shortKey: 'as', encode: encodeAutocomplete, decode: decodeAutocomplete }
+}
+
+/**
+ * Helper method to decode a given paramName from URL parameters base on urlDefs keys
+ * @param {object} params Object of encoded URL parameters
+ * @param {string} paramName Param to decode
+ */
+const decodeHelp = (params, paramName) => {
+    const value = params[urlDefs[paramName].shortKey]
+    return urlDefs[paramName].decode(value)
+}
+
+/**
+ * Given a URL param string, returns an object that matches the redux store
+ * @param {string} paramString a URL encoded parameter string
+ * @return {object} An object of values that match the redux store
+ */
+export const decodeUrlParams = (paramString) => {
+    // decode the paramString
+    const params = qs.parse(paramString, { ignoreQueryPrefix: true, parseArrays: false })
+
+    // build the param object based on the structure in the redux store
+    // e.g. map is store separately from query
+    const focusedGranule = decodeHelp(params, 'focusedGranule')
+
+    const map = decodeHelp(params, 'map')
+
+    const spatial = {}
+    spatial.point = decodeHelp(params, 'pointSearch')
+    spatial.boundingBox = decodeHelp(params, 'boundingBoxSearch')
+    spatial.polygon = decodeHelp(params, 'polygonSearch')
+    spatial.line = decodeHelp(params, 'lineSearch')
+    spatial.circle = decodeHelp(params, 'circleSearch')
+
+    const collectionQuery = { pageNum: 1 }
+    const granuleQuery = { pageNum: 1 }
+    collectionQuery.spatial = spatial
+    collectionQuery.keyword = decodeHelp(params, 'keywordSearch')
+    collectionQuery.temporal = decodeHelp(params, 'temporalSearch')
+    collectionQuery.overrideTemporal = decodeHelp(params, 'overrideTemporalSearch')
+    collectionQuery.gridName = decodeHelp(params, 'gridName')
+    collectionQuery.tagKey = decodeHelp(params, 'tagKey')
+    collectionQuery.hasGranulesOrCwic = decodeHelp(params, 'hasGranulesOrCwic')
+    granuleQuery.gridCoords = decodeHelp(params, 'gridCoords')
+
+    const query = {
+        collection: collectionQuery,
+        granule: granuleQuery
+    }
+
+    const timeline = decodeTimeline(params)
+
+    const featureFacets = decodeHelp(params, 'featureFacets')
+    const granuleDataFormats = decodeHelp(params, 'granuleDataFormatFacets')
+    const instruments = decodeHelp(params, 'instrumentFacets')
+    const organizations = decodeHelp(params, 'organizationFacets')
+    const platforms = decodeHelp(params, 'platformFacets')
+    const processingLevels = decodeHelp(params, 'processingLevelFacets')
+    const projects = decodeHelp(params, 'projectFacets')
+    const scienceKeywords = decodeScienceKeywords(params)
+
+    const {
+        collections,
+        focusedCollection,
+        project
+    } = decodeCollections(params)
+
+    const cmrFacets = {
+        data_center_h: organizations,
+        instrument_h: instruments,
+        granule_data_format_h: granuleDataFormats,
+        platform_h: platforms,
+        processing_level_id_h: processingLevels,
+        project_h: projects,
+        science_keywords_h: scienceKeywords
+    }
+
+    const shapefile = {
+        shapefileId: decodeHelp(params, 'shapefileId')
+    }
+
+    const advancedSearch = decodeAdvancedSearch(params)
+
+    const autocompleteSelected = decodeHelp(params, 'autocompleteSelected')
+
+    // Fetch collections in the project
+    const { collectionIds = [] } = project || {}
+
+    // Create a unique list of collections to fetch and remove any empty values [.filter(Boolean)]
+    const uniqueCollectionList = [...new Set([
+        ...collectionIds,
+        focusedCollection
+    ])].filter(Boolean)
+    // console.log(project, uniqueCollectionList);
+
+    return {
+        advancedSearch,
+        autocompleteSelected,
+        cmrFacets,
+        collections,
+        featureFacets,
+        focusedCollection,
+        focusedGranule,
+        map,
+        conceptId: uniqueCollectionList,
+        query,
+        shapefile,
+        timeline
+    }
+}
