a531cee88cd6b9eecab79228331409a7049096fe
echyam <echyam@gmail.com>
2020-11-03 15:20:17 -0800

    debugging

--- show_ssh_info/show_ssh_info/handlers.py
+++ show_ssh_info/show_ssh_info/handlers.py
@@ -431,7 +431,7 @@ class Presigneds3UrlHandler(IPythonHandler):
         # -----------------------
         # if valid path, get presigned URL
         expiration = '43200' # 12 hrs in seconds
-        logging.debug('expiration is {} seconds'+expiration)
+        logging.debug('expiration is {} seconds', expiration)
 
         url = '{}/api/members/self/presignedUrlS3/{}/{}?exp={}'.format(maap_api_url(self.request.host), bucket, key, expiration)
         headers = {'Accept': 'application/json', 'proxy-ticket': proxy_ticket}
@@ -440,6 +440,7 @@ class Presigneds3UrlHandler(IPythonHandler):
             headers=headers,
             verify=False
         )
+        logging.debug(r.text)
 
         try:
             resp = json.loads(r.text)
