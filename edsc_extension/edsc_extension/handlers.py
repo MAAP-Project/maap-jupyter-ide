10f4493d39b096581969c03a2650ed3fb451fc64
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2021-02-02 18:52:51 -0800

    Don't use the default maap-py api host

--- edsc_extension/edsc_extension/handlers.py
+++ edsc_extension/edsc_extension/handlers.py
@@ -3,9 +3,25 @@ import sys
 import nbformat
 from notebook.base.handlers import IPythonHandler
 import subprocess
+import functools
+import json
 import maap
 from maap.maap import MAAP
 
+@functools.lru_cache(maxsize=128)
+def get_maap_config(host):
+    path_to_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..', 'maap_environments.json')
+
+    with open(path_to_json) as f:
+        data = json.load(f)
+
+    match = next((x for x in data if host in x['ade_server']), None)
+    maap_config = next((x for x in data if x['default_host'] == True), None) if match is None else match
+    
+    return maap_config
+
+def maap_api(host):
+    return get_maap_config(host)['api_server']
 
 class GetGranulesHandler(IPythonHandler):
     def printUrls(self, granules):
@@ -18,7 +34,7 @@ class GetGranulesHandler(IPythonHandler):
 
     def get(self):
 
-        maap = MAAP()
+        maap = MAAP(maap_api(self.request.host))
         cmr_query = self.get_argument('cmr_query', '')
         limit = str(self.get_argument('limit', ''))
         print("cmr_query", cmr_query)
@@ -35,7 +51,7 @@ class GetGranulesHandler(IPythonHandler):
 
 class GetQueryHandler(IPythonHandler):
     def get(self):
-        maap = MAAP()
+        maap = MAAP(maap_api(self.request.host))
         cmr_query = self.get_argument('cmr_query', '')
         limit = str(self.get_argument('limit', ''))
         query_type = self.get_argument('query_type', 'granule')
