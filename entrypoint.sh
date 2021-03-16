c1fe2c33e561fd9b39b9947d0cbb076a8b87348e
George Chang <george.w.chang@jpl.nasa.gov>
2021-03-16 09:00:48 -0700

    Updated entrypoint.sh to make the fix to run Jupyter under Che in 'single-host' mode more generic. Should accommodate Jupyter 2.x & 3.x now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,43 @@ fi
 
 export PREVIEW_URL=`/usr/bin/printenv | grep $THE_MACHINE | perl -slane 'if(/^(SERVER[^_]+_$mn)_SERVICE_PORT=(\d+)/) { $p=$2; print "/".lc($1=~s/_/-/rg)."/server-".$p; }' -- -mn=$THE_MACHINE | uniq`
 
-# Find the jupyter install in conda (because jlab may be installed in python3.6 or python3.7)
+# Fix Jupyterlab for Che in `single-host` mode. In `single-host` mode, Che uses URL path prefixes
+# to distinguish workspaces. So for example, `https://<host>/work/space/path/<jupyter endpoints>`. 
+# Because of this, we need to set Jupyter's `base_url` to `/work/space/path` so that all hrefs
+# and links to files have the correct path prefix. HOWEVER, when the browser accesses those paths,
+# the ingress/nginx proxy strips out the `base_url`! Even if the browser makes a request to `/work/space/path/lab`,
+# Jupyter's web server, Tornado, only see a request for `/lab`. Tornado routes calls to the correct handler by
+# matching the path against a known regular expression. Because `base_url` is configured to `/work/space/path`,
+# Tornado only handles requests that have that path prefix, causing calls such as `/lab` to fail. The fix below
+# allows `base_url` to be set so that HTTP *responses* include the `base_url` so that browsers make the correct
+# call. However, it strips out `base_url` when listening for *requests* so that handles the ingress/nginx proxy
+# requests correctly.
 export NOTEBOOKLIBPATH=`find /opt/conda/lib/ -maxdepth 3 -type d -name "notebook"`
-export JUPYTERLABLIBPATH=`find /opt/conda/lib/ -maxdepth 3 -type d -name "jupyterlab"`
-export JUPYTERLABSERVERLIBPATH=`find /opt/conda/lib/ -maxdepth 3 -type d -name "jupyterlab_server"`
+export JUPYTERSERVERLIBPATH=`find /opt/conda/lib -maxdepth 3 -type d -name "jupyter_server"`
 
-# Fixes to JupyterLab with base_url and Che proxying. Hack.
-perl -pi -e "s|nb_app.web_app.settings\['base_url'\]|'/'|g" $NOTEBOOKLIBPATH/terminal/__init__.py
-perl -pi -e "s|webapp.settings\['base_url'\]|'/'|g" $NOTEBOOKLIBPATH/terminal/__init__.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" $JUPYTERLABLIBPATH/extension.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" $JUPYTERLABSERVERLIBPATH/handlers.py
-perl -pi -e "s|pattern = url_path_join\(settings\['base_url'\], handler\[0\]\)|pattern = url_path_join('/', handler[0])|g" $NOTEBOOKLIBPATH/notebookapp.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /maap-jupyter-ide/pull_projects/pull_projects/__init__.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /maap-jupyter-ide/show_ssh_info/show_ssh_info/__init__.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /maap-jupyter-ide/edsc_extension/edsc_extension/__init__.py
-#perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /jupyterlab_iframe/jupyterlab_iframe/__init__.py
-#perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /inject_ssh/inject_ssh/__init__.py
-perl -pi -e "s|web_app.settings\[\"base_url\"\]|'/'|g" /maap-jupyter-ide/jupyterlab-git/jupyterlab_git/handlers.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /maap-jupyter-ide/submit_jobs/submit_jobs/__init__.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /maap-jupyter-ide/ipycmc/ipycmc/nbextension/__init__.py
-perl -pi -e "s|web_app.settings\['base_url'\]|'/'|g" /maap-jupyter-ide/maapsec/maapsec/__init__.py
+read -r -d '' JUPYTER_PATCH << EOM
+    # Fix for Tornado's inability to handle proxy requests
+    from tornado.routing import _RuleList
+    def fix_handlers(self, handlers: _RuleList, base_url: str):
+        for i in range(len(handlers)):
+            l = list(handlers[i])
+            l[0] = l[0].replace(base_url.rstrip('/'), '')
+            handlers[i] = tuple(l)
+        return handlers
 
+    def add_handlers(self, host_pattern: str, host_handlers: _RuleList) -> None:
+        super().add_handlers(host_pattern, self.fix_handlers(host_handlers, self.settings['base_url']))
+EOM
+
+if [[ -f "$JUPYTERSERVERLIBPATH/serverapp.py" ]]; then
+    perl -pi -e "s|(.*)\(web.Application\):|\$1\(web.Application\):\n$JUPYTER_PATCH|g" "$JUPYTERSERVERLIBPATH/serverapp.py"
+    perl -pi -e 's|(.*)__init__\(handlers(.*)|$1__init__\(self.fix_handlers\(handlers, base_url\)$2|g' "$JUPYTERSERVERLIBPATH/serverapp.py"
+fi
+
+if [[ -f "$NOTEBOOKLIBPATH/notebookapp.py" ]]; then
+    perl -pi -e "s|(.*)\(web.Application\):|\$1\(web.Application\):\n$JUPYTER_PATCH|g" "$NOTEBOOKLIBPATH/notebookapp.py"
+    perl -pi -e 's|(.*)__init__\(handlers(.*)|$1__init__\(self.fix_handlers\(handlers, base_url\)$2|g' "$NOTEBOOKLIBPATH/notebookapp.py"
+fi
 
 # Dump all env variables into file so they exist still though SSH
 env | grep _ >> /etc/environment
@@ -42,4 +58,12 @@ export PATH=$PATH:/opt/conda/bin
 cp /root/.bashrc ~/.bash_profile
 
 service ssh start
-jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --NotebookApp.token='' --LabApp.base_url=$PREVIEW_URL --no-browser --debug
+
+VERSION=$(jupyter lab --version)
+if [[ $VERSION > '2' ]] && [[ $VERSION < '3' ]]; then
+    jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --NotebookApp.token='' --NotebookApp.base_url=$PREVIEW_URL --no-browser --debug
+elif [[ $VERSION > '3' ]] && [[ $VERSION < '4' ]]; then
+    jupyter lab --ip=0.0.0.0 --port=3100 --allow-root --ServerApp.token='' --ServerApp.base_url=$PREVIEW_URL --no-browser --debug
+else
+    # error
+fi
