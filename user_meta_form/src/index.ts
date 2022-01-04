d8381e282aab8077311e30281abd4a428ec535eb
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2022-01-04 13:46:21 -0800

    downgraded to jupyter v2

--- user_meta_form/src/index.ts
+++ user_meta_form/src/index.ts
@@ -1,37 +1,3 @@
-// import {
-//   JupyterFrontEnd, JupyterFrontEndPlugin
-// } from '@jupyterlab/application';
-
-// import {
-//   ICommandPalette
-// } from '@jupyterlab/apputils';
-
-// const extension: JupyterFrontEndPlugin<void> = { 
-//   id: 'user-metadata-form',
-//   requires: [ICommandPalette],
-//   autoStart: true,
-//   activate (app: JupyterFrontEnd, palette: ICommandPalette): void {
-//     /**
-//      * Initialization data for the jupyterlab-ext extension.
-//      */
-
-// // mainMenu.addMenu(menu, { rank: 40 });
-// //     app.commands.addCommand(command, {
-// //       label: 'Share your own data',
-// //       execute: () => {
-// //         const url = 'https://questionnaire.maap-project.org/';
-// //         window.open(url)
-// //       }
-// //     });
-
-// //     palette.addItem({ command: command, category: 'Metadata' });
-//     console.log("user_meta_form activated");
-
-//   }
-// };
-
-// export default extension;
-
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
@@ -65,7 +31,7 @@ const extension: JupyterFrontEndPlugin<void> = {
     });
 
     // Add the command to the command palette
-    const category = 'Extension Examples';
+    const category = 'Metadata';
     palette.addItem({ command, category, args: { origin: 'from palette' } });
   },
 };
